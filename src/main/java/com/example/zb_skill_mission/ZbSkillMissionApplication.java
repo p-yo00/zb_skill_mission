beb126b81682d1399d1b28e86486a3fe328c34a1
yeon <p-yo00@naver.com>
2024-05-23 21:10:03 +0900

    initial commit

---
+++ src/main/java/com/example/zb_skill_mission/ZbSkillMissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.zb_skill_mission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbSkillMissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZbSkillMissionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zb_skill_mission/ZbSkillMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zb_skill_mission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZbSkillMissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
